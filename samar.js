a747bbc736bce1d53647fd3def9c4af93b86f37d
samar pratap yadav <66524944+SAMARpratapYADAV@users.noreply.github.com>
2023-01-31 22:27:32 +0530

    the clone directory is upgraded

--- samar.js
+++ samar.js
@@ -1,6 +1,6 @@
 console.log("hello js working")
-function samar(a, b){
-    c=a*b;
-    return(c);
+function samar(){
+    let y =document.getElementById('inp1').value;
+    document.getElementById('div1').innerHTML=y;
 }
 //document.getElementById('div1').innerHTML=samar(2,3);
